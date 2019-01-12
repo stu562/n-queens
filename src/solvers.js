c5dbf5cb82a484098bdc590dd1492c62706efa08
stu562 <stu562@gmail.com>
2019-01-12 12:33:58 -0800

    Worked on findNRooksSolution

--- src/solvers.js
+++ src/solvers.js
@@ -14,30 +14,67 @@ console.log(row[diagonalIndex + j - r] );
 // return a matrix (an array of arrays) representing a single nxn chessboard, 
 //with n rooks placed such that none of them can attack each other
 
-
-
 window.findNRooksSolution = function(n) {
   var solution = []; //fixme
-  var board = new Board({n:n});
-  var board3 = new Board({n:8});
+  var board = new Board({'n':n});
   var rowCount = 0;
   var colCount = 0;
-  
-  var matrix = board.attributes;
-  //iterate through matrix board 
-  for(var rowIndex = 0; rowIndex < matrix.length; rowIndex++){
-    for(var colIndex = 0; colIndex < matrix[rowIndex].length; colIndex++){
-      //test each spot 
-      if (this.hasAnyRooksConflicts) {
-        // if (hasConf) => try next space 
-        continue;
-      } else {
-        this.togglePiece(rowIndex, colIndex);
-        // else replace 0 with 1 
-      }
+   // setup matrix board 
+  var matrix = new Array(n);
+  for ( var i = 0; i < matrix.length; i++) {
+    matrix[i] = new Array(n);
+  };
+  for (var i = 0; i < matrix.length; i++) {
+    for (var j = 0; j < matrix[i].length; j++) {
+      console.log('elem',matrix[i][j])
+      matrix[i][j] = 0;
     }
   }
-  
+  console.log('brd', matrix)
+  // if ( n === 1) {return [[1]]}
+
+  // setup matrix board 
+  // for (var key in board.attributes) {
+  //   console.log('ey',key)
+  //   if (key !== 'n') {
+  //     matrix.push(board.attributes[key]);
+  //   }
+  // }
+  console.log('initial matrix', matrix);
+  for(var rowIndex = 0; rowIndex < matrix[0].length; rowIndex++){
+    for(var colIndex = 0; colIndex < matrix[0].length; colIndex++){
+      //test each spot
+      // toggle
+      matrix[rowIndex][colIndex] = 1
+      
+      // Append matrix to board
+      for (var key in board.attributes) {
+        if (key !== 'n') {
+          board.attributes[key] = matrix[Number(key)];
+        }
+      }
+      console.log(matrix, 'Im bored')
+      // if (hasConf) => try next space 
+      if (board.hasAnyRooksConflicts()) {
+        console.log('CONFLICT', board.attributes);
+        
+        // matrix[rowIndex][colIndex] = 0;
+
+        // Append matrix to board
+        for (var key in board.attributes) {
+          console.log('dont worry', matrix[Number(key)]);
+          if (key !== 'n') {
+            board.attributes[key] = matrix[Number(key)];
+          }
+        }
+        rowIndex++;
+        console.log('rowIndex:', rowIndex, matrix);
+      } 
+    }
+    // console.log('matrix', matrix);
+    // return matrix;
+  }
+  solution = matrix;
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
   return solution;
 };
